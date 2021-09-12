90475a30b52cc63c9b7eff07a2ffd3620a0ad6c8
Shane Boer <shane2511@live.nl>
2021-09-12 21:57:31 +0200

    commit py files

---
+++ de-speelhal-dag.py
@@ -0,0 +1,10 @@
+entreekosten = 7.45
+vipVR = 0.37/5
+deeltijd = 45
+personen = 3
+factuurkosten = 'dit geweldige dagje uit met 4 mensen in de peelhal met 45 minuten VR kost je maar ' + str((personen) * (entreekosten) + (deeltijd) * (vipVR)) + ' euro'
+
+print(factuurkosten)
+
+
+
